f0ba8adf482520d0db5f4122fd62c78741f54931
Norma Jean <cdm0971@gmail.com>
2023-02-27 11:47:46 +0600

    First commit

---
+++ midterm/blogs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    owner = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
---
+++ midterm/blogs/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Blog
+from .serializers import BlogSerializer
+
+class BlogList(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
